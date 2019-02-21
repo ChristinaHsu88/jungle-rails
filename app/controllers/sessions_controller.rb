b0f94922eacd84cd20bd8ec6be1c729a95e23666
vagrant <christina.hsu.ca@gmail.com>
2019-02-21 20:29:11 +0000

    add user authentication method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    #if the user exists and the password matches
+    if user && user.authenticate(params[:params])
+      #save the user id inside the browser cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      #wrong login, redirect to the login form
+      redirect_to '/login'
+    end
   end 
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
       redirect_to '/'
     else
       redirect_to '/signup'
+    end
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
