e5f7702900c8bf5ba158f94651cde2db787da9ad
vagrant <christina.hsu.ca@gmail.com>
2019-02-21 06:15:31 +0000

    add partials/categories page

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,37 @@
+class Admin::CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+    end
+
+    # def new
+    #     @category = Category.new
+    # end
+
+    # def create
+        # @category = Category.new(category_params)
+        # if @category.save
+        #     redirect_to [:admin, :categories], notice: 'Category created!'
+        # else
+        #     render :new
+        # end
+    # end
+   
+
+end
+
+
+# resources :categories, only: [:index, :create, :new]
+
+
+#   private
+
+#   def product_params
+#     params.require(:product).permit(
+#       :name,
+#       :description,
+#       :category_id,
+#       :quantity,
+#       :image,
+#       :price
+#     )
+#   end
