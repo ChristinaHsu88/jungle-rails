baed37ff5e71f0dce33670e8e15dcb4f2e642d3b
vagrant <christina.hsu.ca@gmail.com>
2019-02-24 04:26:22 +0000

    add review and render in the controller page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,21 @@
 class ReviewsController < ApplicationController
     def create
-        # @review = Review.create()
-        # # @review.user = current_user
-        # if @review.save
-        # redirect_to @review
-        # end
+        #create an instance
+      review = Review.new(review_params)
+        #store the session id in the review table. Params allow to access all the parameters passed in
+      review.user_id = session[:user_id]
+      review.product_id = params[:product_id]
+      if review.save
+        review.save
+        redirect_to "/products/#{params[:product_id]}"
+      else
+        redirect_to "/products/#{params[:prouct_id]}"
+      end
     end
 
+    private
+    #require(object), permit(the nested objects being passed in)
+        def review_params
+            params.require(:review).permit(:rating, :description)
+        end
 end
